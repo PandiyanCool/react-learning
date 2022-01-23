c1040324266412980497d1b7adc0df50ed2b191a
Pandiyan Murugan <pandiyan.murugan@kovai.co>
2022-01-23 12:18:33 +0530

    Updated usage

--- src/main-page/house-filter.js
+++ src/main-page/house-filter.js
@@ -2,7 +2,7 @@ import { useNavigate } from "react-router-dom";
 
 const HouseFilter = ({ allHouses }) => {
   const history = useNavigate();
-  
+
   const countries = allHouses
     ? Array.from(new Set(allHouses.map((x) => x.country)))
     : [];
@@ -22,11 +22,11 @@ const HouseFilter = ({ allHouses }) => {
       </div>
       <div className="col-md-4 mb-3">
         <select className="form-select" onChange={onSearchChange}>
-          {countries.map((m) => {
+          {countries.map((m) => (
             <option key={m} value={m}>
               {m}
-            </option>;
-          })}
+            </option>
+          ))}
         </select>
       </div>
     </div>
