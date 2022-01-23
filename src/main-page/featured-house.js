5a365b72cbbd7fe38a1f9fbebf666b4518f15403
Pandiyan Murugan <pandiyan.murugan@kovai.co>
2022-01-23 13:25:05 +0530

    added css

--- src/main-page/featured-house.js
+++ src/main-page/featured-house.js
@@ -3,7 +3,7 @@ import House from "../house";
 const FeaturedHouse = ({ house }) => {
   if (house) {
     return (
-      <div>
+      <div className="container">
         <div className="row featureHouse">
           <h3 className="col-md-12 text-center">Featured house</h3>
         </div>
